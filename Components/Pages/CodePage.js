7513f7f6bf0dd96302a2bc51aca44aa6b8b2a0d8
gavin <gallmond@gmail.com>
2022-06-02 08:24:42 +0100

    got a QR code to appear

--- Components/Pages/CodePage.js
+++ Components/Pages/CodePage.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
+
+const generateQrCode = () => {
+  return 'Nayab is cool they are my pal :)'
+}
+
 
 const CodePage = () => {
 
   return (<View style={styles.container}>
     <Text style={{...styles.text, marginBottom: 5}}>This is your code.</Text>
-    <Text style={styles.text}>Your partner should tap 'Scan a code' and scan this code!</Text>
+    <Text style={styles.text}>Yossur partner should tap 'Scan a code' and scan this code!</Text>
 
+    <View style={styles.qrContainer}>
+    <QRCode
+     value={generateQrCode()}
+     size={250}
+     />
+    </View>
 
   </View>)
 
 }
 
 const styles = StyleSheet.create({
+  qrContainer:{
+    marginTop: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
   container:{
 
   },
