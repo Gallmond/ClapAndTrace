065e3cbe3338c093dd851cc1514648bb2ff6e25d
gavin <gallmond@gmail.com>
2022-06-08 08:57:24 +0100

    hashes fingerprint

--- Components/Pages/CodePage.js
+++ Components/Pages/CodePage.js
@@ -1,70 +1,79 @@
 import React from 'react';
-import { View, StyleSheet, Text, NativeModules } from 'react-native';
+import {View, StyleSheet, Text, NativeModules} from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
-import { DATA_PREFIX } from '../../Helpers/Constants';
+import {DATA_PREFIX} from '../../Helpers/Constants';
+import { JSHash, JSHmac, CONSTANTS } from "react-native-hash";
 
-const generateQrCode = () => {
+const hashString = async ( string ) => {
+  const hash = await JSHash("message", CONSTANTS.HashAlgorithms.sha256);
+  return hash;
+}
+
+const generateQrCodeData = async () => {
   /**
    * in the 'real world' we would save this to local devices 'what I said' codes
    * so if the user then reports a positive test, we can send all the codes used
    * after the positive date off to the remote server as 'positive cases said'
    * codes for other phones to check.
-   * 
+   *
    * We'll just pretend though.
    */
 
-  const fingerPrint = NativeModules?.PlatformConstants?.getConstants()?.Fingerprint
-    ?? NativeModules?.PlatformConstants?.getAndroidID() ?? ''
+  let fingerPrint =
+    NativeModules?.PlatformConstants?.getConstants()?.Fingerprint ??
+    NativeModules?.PlatformConstants?.getAndroidID() ??
+    '';
 
-  if(fingerPrint === ''){
-    //TODO[gavin] handle unable to create fingerprint
+  if (fingerPrint === '') {
+    setCanDisplay(false);
+    return '';
   }
 
-  //TODO[gavin] hash fingerprint
-  const hashedFingerprint = fingerPrint.replace(/\:/g, '-');
+  fingerPrint = fingerPrint.replace(/\:/g, '-');
+  console.log('preHash', fingerPrint);
+  const hashedFingerprint = await hashString( fingerPrint )
+  console.log('hashedFingerprint', hashedFingerprint);
 
   const todayYYYYMMDD = new Date().toISOString().split('T')[0];
 
-  const salt = Math.floor(Math.random() * 1000000).toString(32)
+  const salt = Math.floor(Math.random() * 1000000).toString(32);
 
-  const dataParts = [
-    DATA_PREFIX,
-    todayYYYYMMDD,
-    hashedFingerprint,
-    salt
-  ];
+  const dataParts = [DATA_PREFIX, todayYYYYMMDD, hashedFingerprint, salt];
 
   const dataString = dataParts.join(':');
+  console.log('dataString', dataString);
 
   return dataString;
-}
+};
 
 const CodePage = () => {
+    const [codeData, setCodeData] = React.useState(null);
 
-return (<View style={styles.container}>
-    <Text style={{...styles.text, marginBottom: 5}}>This is your code.</Text>
-    <Text style={styles.text}>Your partner should tap 'Scan a code' and scan this code!</Text>
+  React.useEffect(()=>{
+    // uses async hashing, so has to be useEffect.
+    generateQrCodeData().then(setCodeData);
+  }, [])
 
-    <View style={styles.qrContainer}>
-    <QRCode
-     value={generateQrCode()}
-     size={250}
-     />
+  return (
+    <View style={styles.container}>
+      <Text style={{...styles.text, marginBottom: 5}}>This is your code.</Text>
+      <Text style={styles.text}>
+        Your partner should tap 'Scan a code' and scan this code!
+      </Text>
+      <View style={styles.qrContainer}>
+        {codeData && <QRCode value={codeData} size={250} />}
+      </View>
     </View>
-
-  </View>)
-
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  qrContainer:{
+  qrContainer: {
     marginTop: 20,
     marginLeft: 'auto',
     marginRight: 'auto',
   },
-  container:{
-
-  },
+  container: {},
   text: {
     textAlign: 'center',
     color: 'black',
@@ -72,8 +81,7 @@ const styles = StyleSheet.create({
     fontSize: 17,
     marginLeft: 5,
     marginRight: 5,
-  }
-
+  },
 });
 
 export default CodePage;
---
+++ README.md
@@ -0,0 +1,5 @@
+# remaining
+- //TODO hash the fingerprint
+- //TODO handle unable to generate fingerprint
+- //TODO loading spinner on the pretend check and report submissions
+
